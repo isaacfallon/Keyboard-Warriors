fb6f5ffba019c4cd362f11d2f5855dc67836891d
isaacfallon <isaac.fallon@gmail.com>
2024-10-15 17:21:47 +1030

    Added maths module and fixed some minor design issues

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,16 +11,16 @@ export default function Footer() {
             <ul className="flex justify-end flex-row mt-6">
                 <li className="px-4"><a href="https://isaacfallon.com/" target="_blank" rel="noreferrer" className="hover:text-sky-700">Made by Isaac Fallon</a></li>
                 <li className="px-4"><a href="https://github.com/isaacfallon/Project-3-Keyboard-Warriors" target="_blank" rel="noreferrer" className="hover:text-sky-700">GitHub</a></li>
-                <li className="px-4 hover:text-sky-700 hover:cursor-pointer" onClick={() => setOpen(true)}>v2.1</li>
+                <li className="px-4 hover:text-sky-700 hover:cursor-pointer" onClick={() => setOpen(true)}>v2.2</li>
                 <Modal open={open} onClose={() => setOpen(false)}>
                     <div className="mx-auto text-center my-4 w-[600px] p-4">
-                        <h3 className="text-3xl font-black mb-2">Version 2.1 Patch Notes (20/09/24):</h3>
+                        <h3 className="text-3xl font-black mb-2">Version 2.2 Patch Notes (15/10/24):</h3>
                         <div className="text-left">
                             <ul>
-                                <li className="list-disc ml-4">Entire visual design revamp with Tailwind CSS (including darkmode and theme switch toggle)</li>
+                                <li className="list-disc ml-4">Added a maths testing module to replicate the popular &apos;Mathletics&apos; program used to teach in schools. Check it out by using the toggle at the top right!</li>
+                                <li className="list-disc ml-4">Visual design cleaned up throughout game areas after identifying some issues from the Tailwind overhaul last patch.</li>
                                 <li className="list-disc ml-4">Improved profile page flow to reduce clutter if the user had a large amount of results saved. If the user now has more than 5 results saved, these excess results are saved in an openable menu further down their profile page.</li>
-                                <li className="list-disc ml-4">Fixed a bug where the user could continue typing and increase their score after the timer had reached 0.</li>
-                                <li className="list-disc ml-4">Fixed a bug where the user could break the game by clicking into the &apos;start game&apos; field once a game was in progress. Now if a user clicks away they will need to restart a game manually. (I am considering forcing a reset if any mouse clicking activity is detected but will need to test this). </li>
+                               
                             </ul>
                         </div>
                     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,16 @@
-
 import GameArea from '../components/GameArea';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  
-  return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
 
-        >
+  return (
+      <div className="flex-row justify-center">
+        <div className="col-12 col-md-10 mb-3 p-3">
+        <Link to="/maths"><button className="absolute top-2 right-48 px-12 bg-zinc-600 dark:bg-gray-200 text-gray-100 dark:text-zinc-600 font-bold px-4 rounded">Switch to maths</button></Link>
+
           <GameArea />
         </div>
       </div>
-    </main>
   );
 };
 
